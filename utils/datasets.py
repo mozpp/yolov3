c861c761efcb2c62e805342cb4dbae0fb9867ebe
moz <mozpp@foxmail.com>
2019-11-12 15:04:48 +0800

    update

--- utils/datasets.py
+++ utils/datasets.py
@@ -804,6 +804,13 @@ def convert_images2bmp():
         with open(label_path.replace('5k', '5k_bmp'), 'w') as file:
             file.write(lines)
 
+def imagelist2folder(path='../data/sm3/out_test.txt'):  # from utils.datasets import *; imagelist2folder()
+    # Copies all the images in a text file (list of images) into a folder
+    create_folder(path[:-4])
+    with open(path, 'r') as f:
+        for line in f.read().splitlines():
+            os.system('cp "%s" %s' % (line, path[:-4]))
+            print(line)
 
 def create_folder(path='./new_folder'):
     # Create folder
